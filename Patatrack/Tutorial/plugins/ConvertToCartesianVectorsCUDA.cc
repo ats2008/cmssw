0bd010d28855ef71c5dae6b98d856fee785bdb43
aravind sugunan <aravindsugunan@gmail.com>
2020-06-22 20:12:20 +0200

    soln exercise 4

--- Patatrack/Tutorial/plugins/ConvertToCartesianVectorsCUDA.cc
+++ Patatrack/Tutorial/plugins/ConvertToCartesianVectorsCUDA.cc
@@ -13,9 +13,13 @@
 #include "FWCore/Utilities/interface/StreamID.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
 
+#include "HeterogeneousCore/CUDAUtilities/interface/host_unique_ptr.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/device_unique_ptr.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/host_noncached_unique_ptr.h"
+#include "HeterogeneousCore/CUDACore/interface/ScopedContext.h"
 #include "cudavectors.h"
 
-class ConvertToCartesianVectorsCUDA : public edm::stream::EDProducer<> {
+class ConvertToCartesianVectorsCUDA : public edm::stream::EDProducer<edm::ExternalWork> {
 public:
   explicit ConvertToCartesianVectorsCUDA(const edm::ParameterSet&);
   ~ConvertToCartesianVectorsCUDA() = default;
@@ -26,8 +30,9 @@ private:
   using CartesianVectors = std::vector<math::XYZVectorF>;
   using CylindricalVectors = std::vector<math::RhoEtaPhiVectorF>;
 
+  void acquire(edm::Event const& event, edm::EventSetup const& setup, edm::WaitingTaskWithArenaHolder waitingTaskHolder) override;
   virtual void produce(edm::Event&, const edm::EventSetup&) override;
-
+  cms::cuda::host::unique_ptr<cudavectors::CartesianVector[]> output_buffer_;
   edm::EDGetTokenT<CylindricalVectors> input_;
   edm::EDPutTokenT<CartesianVectors> output_;
 };
@@ -37,27 +42,64 @@ ConvertToCartesianVectorsCUDA::ConvertToCartesianVectorsCUDA(const edm::Paramete
   output_ = produces<CartesianVectors>();
 }
 
-void ConvertToCartesianVectorsCUDA::produce(edm::Event& event, const edm::EventSetup& setup) {
+//void ConvertToCartesianVectorsCUDA::produce(edm::Event& event, const edm::EventSetup& setup) {
+ 
+void ConvertToCartesianVectorsCUDA::acquire(const edm::Event& event, const edm::EventSetup& setup,
+                                                           edm::WaitingTaskWithArenaHolder waitingTaskHolder) {
+
+  cms::cuda::ScopedContextAcquire ctx{event.streamID(), std::move(waitingTaskHolder)};
+
   auto const& input = event.get(input_);
   auto elements = input.size();
-  auto product = std::make_unique<CartesianVectors>(elements);
+
+ //  auto product = std::make_unique<CartesianVectors>(elements);
 
   // allocate memory on the GPU for the cylindrical and cartesian vectors
-  // fill here ...
-
+  
+  //cudavectors::CylindricalVector* gpu_input;
+  //cudavectors::CartesianVector* gpu_product;
+  //cudaCheck(cudaMalloc(&gpu_input, sizeof(cudavectors::CylindricalVector) * elements));
+  //cudaCheck(cudaMalloc(&gpu_product, sizeof(cudavectors::CartesianVector) * elements));
+ 
+  auto gpu_input = cms::cuda::make_device_unique<cudavectors::CylindricalVector[]>(elements, ctx.stream());
+  auto gpu_product = cms::cuda::make_device_unique<cudavectors::CartesianVector[]>(elements, ctx.stream());
+  auto cpu_input = cms::cuda::make_host_noncached_unique<cudavectors::CylindricalVector[]>(elements, cudaHostAllocWriteCombined);
+  output_buffer_ = cms::cuda::make_host_unique<cudavectors::CartesianVector[]>(elements, ctx.stream());
   // copy the input data to the GPU
-  // fill here ...
+  
+  std::memcpy(cpu_input.get(), input.data(), sizeof(cudavectors::CylindricalVector) * elements);
+  cudaCheck(cudaMemcpyAsync(gpu_input.get(), cpu_input.get(), sizeof(cudavectors::CylindricalVector) * elements, cudaMemcpyHostToDevice, ctx.stream()));
 
-  // convert the vectors from cylindrical to cartesian coordinates, on the GPU
-  // fill here ...
+ //  cudaCheck(cudaMemcpy(gpu_input.get(), cpu_input.get(), sizeof(cudavectors::CylindricalVector) * elements, cudaMemcpyHostToDevice));
+ //    cudaCheck(cudaMemcpy(gpu_input, input.data(), sizeof(cudavectors::CylindricalVector) * elements, cudaMemcpyHostToDevice));
 
+   // convert the vectors from cylindrical to cartesian coordinates, on the GPU
+  
+   cudavectors::convertWrapper(gpu_input.get(), gpu_product.get(), elements, ctx.stream());
+  // cudavectors::convertWrapper(gpu_input.get(), gpu_product.get(), elements);
+  // cudavectors::convertWrapper(gpu_input, gpu_product, elements);
+  
   // copy the result from the GPU
-  // fill here ...
-
-  // free the GPU memory
-  // fill here ...
-
-  event.put(output_, std::move(product));
+  
+ cudaCheck(cudaMemcpyAsync(output_buffer_.get(), gpu_product.get(), sizeof(cudavectors::CartesianVector) * elements, cudaMemcpyDeviceToHost, ctx.stream()));
+  // cudaCheck(cudaMemcpy(product->data(), gpu_product.get(), sizeof(cudavectors::CartesianVector) * elements, cudaMemcpyDeviceToHost));
+  // cudaCheck(cudaMemcpy(product->data(), gpu_product, sizeof(cudavectors::CartesianVector) * elements, cudaMemcpyDeviceToHost));
+  
+ // free the GPU memory
+ // cudaCheck(cudaFree(gpu_input));
+ // cudaCheck(cudaFree(gpu_product));
+ } 
+void ConvertToCartesianVectorsCUDA::produce(edm::Event& event, const edm::EventSetup& setup) {
+  //no need for a CUDA context here, because there are no CUDA operations
+ 
+   auto const& input = event.get(input_);
+   auto elements = input.size();
+  
+  // instantiate the event product, copy the results from the output buffer, and free it
+   auto product = std::make_unique<CartesianVectors>(elements);
+   std::memcpy((void*) product->data(), output_buffer_.get(), sizeof(cudavectors::CartesianVector) * elements);
+   output_buffer_.reset();
+   event.put(output_, std::move(product));
 }
 
 void ConvertToCartesianVectorsCUDA::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -65,6 +107,5 @@ void ConvertToCartesianVectorsCUDA::fillDescriptions(edm::ConfigurationDescripti
   desc.add<edm::InputTag>("input", edm::InputTag("cylindricalVectors"));
   descriptions.addWithDefaultLabel(desc);
 }
-
 // define this as a plug-in
 DEFINE_FWK_MODULE(ConvertToCartesianVectorsCUDA);
