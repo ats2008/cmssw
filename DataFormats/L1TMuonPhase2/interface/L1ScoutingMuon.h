7408d7111d6d8ea814ae308b0f87188231075953
Aravind <aravindsugunan@gmail.com>
2024-08-14 18:30:25 +0200

    Adding the flat table producer

--- DataFormats/L1TMuonPhase2/interface/L1ScoutingMuon.h
+++ DataFormats/L1TMuonPhase2/interface/L1ScoutingMuon.h
@@ -12,14 +12,19 @@ namespace l1Scouting {
     Muon() {}
     Muon(float pt, float eta, float phi, float z0, float d0)
         : pt_(pt), eta_(eta), phi_(phi), z0_(z0), d0_(d0) {}
+    Muon(float pt,float eta,float phi, float z0,float d0,int8_t charge,uint8_t quality,float beta,uint8_t isolation)
+        : pt_(pt), eta_(eta), phi_(phi), z0_(z0), d0_(d0) , charge_(charge) , quality_(quality), beta_(beta),isolation_(isolation) {}
+        
 
     float pt() const { return pt_; }
     float eta() const { return eta_; }
     float phi() const { return phi_; }
     float z0() const { return z0_; }
     float d0() const { return d0_; }
+    float beta() const { return beta_; }
+     int8_t charge() const { return charge_; }
     uint8_t quality() const { return quality_; }
-    int charge() const { return charge_; }
+    uint8_t isolation() const { return isolation_; }
 
     void setPt(float pt) { pt_ = pt; }
     void setEta(float eta) { eta_ = eta; }
@@ -33,8 +38,10 @@ namespace l1Scouting {
 
   private:
     float pt_, eta_, phi_, z0_, d0_;
-    uint8_t  quality_;
     int8_t charge_;
+    uint8_t  quality_;
+    float beta_;
+    uint8_t isolation_;
 
   };
 
